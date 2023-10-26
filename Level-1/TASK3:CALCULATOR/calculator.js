9c9e7be33f9b51e827f924ff231c23c8e461a199
CHANDAN SATVIK VAKA <chandansatvik@onmail.com>
2023-10-26 18:05:01 +0530

    Update calculator.js

--- Level-1/TASK3:CALCULATOR/calculator.js
+++ Level-1/TASK3:CALCULATOR/calculator.js
@@ -1,17 +1,37 @@
-let display = document.getElementById('display');
 
-function appendToDisplay(value) {
+function updateDisplay(value) {
+    const display = document.getElementById('display');
     display.value += value;
-}
+  }
+  
 
-function clearDisplay() {
+  function clearDisplay() {
+    const display = document.getElementById('display');
     display.value = '';
-}
+  }
+  
 
-function calculateResult() {
+  function calculateResult() {
+    const display = document.getElementById('display');
     try {
-        display.value = eval(display.value);
+      display.value = eval(display.value);
     } catch (error) {
-        display.value = 'Error';
+      display.value = 'Error';
     }
-}
+  }
+  
+
+  const buttons = document.querySelectorAll('.button');
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const value = button.textContent;
+      if (value === '=') {
+        calculateResult();
+      } else if (value === 'C') {
+        clearDisplay();
+      } else {
+        updateDisplay(value);
+      }
+    });
+  });
+  
